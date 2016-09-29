8d2ac20cd75c90b9edb6a1eec7c4ca5201269383
James Schiiller <jgschiiller@gmail.com>
2016-09-29 02:02:24 -0400

    Update prog.rb

--- prog.rb
+++ prog.rb
@@ -4,35 +4,35 @@
 # count through frequency table, assigning order numbers to every frequency bin
 
 def p()
-	n = 10 # dimension array size n
-	max = 6 # dimension max
-	a = []
-	a[1] = 3
-	a[2] = 1
-	a[3] = 2
-	a[4] = 6
-	a[5] = 5
-	a[6] = 6
-	a[7] = 1
-	a[8] = 1
-	a[9] = 6
-	a[10] = 0
+  n = 10 # dimension array size n
+  max = 6 # dimension max
+  a = []
+  a[1] = 3
+  a[2] = 1
+  a[3] = 2
+  a[4] = 6
+  a[5] = 5
+  a[6] = 6
+  a[7] = 1
+  a[8] = 1
+  a[9] = 6
+  a[10] = 0
   c = Array.new(10)
 
-	for i in 1..n
+  for i in 1..n
     c[a[i]] = c[a[i]].to_i + 1
-	end
+  end
 
-	k = 1
+  k = 1
 
-	for j in 0..max # loop through frequency table
+  for j in 0..max # loop through frequency table
     for i in 1..c[j].to_i # j is number analyzing at the moment
       a[k] = j
-    	k = k + 1
+      k = k + 1
     end
-	end
-
-	puts a
+  end
+	
+  puts a
 end
 
 p()
